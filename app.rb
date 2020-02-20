f3cda10de003a6c3d329ac339710ac3599d2e103
wags83 <gpwagner@gmail.com>
2020-02-20 13:28:30 -0500

    test

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+
+    get '/new' do
+    end
+
+    post '/puppy' do
+    end
+    
+
 end
